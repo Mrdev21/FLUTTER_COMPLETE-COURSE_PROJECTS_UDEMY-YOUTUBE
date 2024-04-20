c7ed9bd76ba7f7f7c09216b647d2b5c3015fb360
Himanshu Chaudhari <148252440+Mrdev21@users.noreply.github.com>
2024-04-20 18:44:57 +0530

    Add files via upload

---
+++ I_AM_RICH_FLUTTER/android/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Center(
+            child: Image(
+              image: AssetImage('images/diamond.png'),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
